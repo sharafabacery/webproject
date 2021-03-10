a53681cafdf616006e4fe1447b07552af353af28
sharafabacery <52586357+sharafabacery@users.noreply.github.com>
2021-03-10 04:02:48 +0200

    add sms

---
+++ sms-php/test.php
@@ -0,0 +1,11 @@
+<?php 
+require 'vendor/autoload.php';
+
+use GuzzleHttp\Psr7\Request;
+
+$client = new \GuzzleHttp\Client();
+$response = $client->request('POST', 'https://smsmisr.com/api/webapi/?username=FHV9ce5E&password=4OLWygYhxw&language=1&sender=Offers&mobile=201018184170&message=hellophpguzzle');
+echo $response->getStatusCode(); // 200
+
+
+?>
