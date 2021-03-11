d5672f26dd1ffc8513d6abd2786e0180e6db819f
sharafabacery <52586357+sharafabacery@users.noreply.github.com>
2021-03-11 21:15:00 +0200

    php

--- sms-php/test.php
+++ sms-php/test.php
@@ -4,6 +4,8 @@ require 'vendor/autoload.php';
 use GuzzleHttp\Psr7\Request;
 
 $client = new \GuzzleHttp\Client();
+//http://localhost/webproject/
+//welcome patient
 $response = $client->request('POST', 'https://smsmisr.com/api/webapi/?username=FHV9ce5E&password=4OLWygYhxw&language=1&sender=Offers&mobile=201018184170&message=hellophpguzzle');
 echo $response->getStatusCode(); // 200
 
